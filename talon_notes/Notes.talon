6572a60c391f67d8475c0f4736a2b96b18c63102
colton <cloftus@princeton.edu>
2022-05-07 19:41:13 -0400

    switch notes to db instead of txt

--- talon_notes/Notes.talon
+++ talon_notes/Notes.talon
@@ -1,8 +1,15 @@
 mode: command
 -
-erase note:
-     user.erase_note()
+
+erase note <digits>:
+     user.erase_note("{digits_1}")
 ^pencil note <phrase>$:
      user.add_note(phrase)
 toggle Notes:
      user.toggle_notes()
+export notes:
+     user.export_notes()
+trash notes:
+     user.trash_note()
+recompute:
+     user.recompute_primary_key()
--- talon_notes/notes.py
+++ talon_notes/notes.py
@@ -1,18 +1,30 @@
 
 from talon import Module, actions, imgui
 import os
+import sqlite3
 
 mod = Module()
-path = os.path.dirname(os.path.realpath(__file__)) + "/notes.txt"
-
+db = os.path.dirname(os.path.realpath(__file__)) + "/notes.db"
+text_path = os.path.dirname(os.path.realpath(__file__)) + "/notes.txt"
 
 @imgui.open()
 def gui(gui: imgui.GUI):
+
     gui.text("Notes")
     gui.line()
-    with open(os.path.expanduser(path), "r") as f:
-        for line in f.readlines():
-            gui.text(line.strip())
+
+    # if database doesn't exist, create it
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY,
+        note TEXT
+    )""")
+    # print all rows in table
+    c.execute("SELECT * FROM notes")
+    rows = c.fetchall()
+    for row in rows:
+        gui.text(f'{row[0]}: {row[1]}')
 
     gui.spacer()
     if gui.button("Close"):
@@ -22,29 +34,72 @@ def gui(gui: imgui.GUI):
 class Actions:
     def add_note(text: str):
         """Adds a note to the GUI"""
-        # get number of lines in file
 
-        with open(path, "a+") as f:
-            # get number of lines in file
-            f.write(f"\n{text}")
-        # add str to gui
+        # add string to database
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY,
+            note TEXT
+        )""")
+        c.execute("INSERT INTO notes (note) VALUES (?)", (text,))
+        conn.commit()
+        conn.close()
+
+        gui.hide()
         gui.show()
 
-    def erase_note():
+
+    def erase_note(primary_key: str):
         """Removes a note from the GUI"""
-        gui.hide()
         try:
-            os.remove(path)
+            conn = sqlite3.connect(db)
+            c = conn.cursor()
+            c.execute("DELETE FROM notes WHERE id = ?", (primary_key,))
+            conn.commit()
+
+            """Recomputes the primary key for the database"""
+            c.execute("SELECT * FROM notes")
+            rows = c.fetchall()
+
+            # enumerate rows
+            for index, row in enumerate(rows):
+                c.execute("UPDATE = ? WHERE id = ?", (index, row[0]))
+            conn.commit()
+            conn.close()        
+        except Exception as e:
+            print(e)
+        if gui.showing:
+            # Hiding then showing prevents visual bugs
+            gui.hide()
+            gui.show()
+
+    def trash_note():
+        """Removes all notes from the GUI"""
+        try:
+            os.remove(db)
         except FileNotFoundError:
             print("No notes to erase")
 
     def toggle_notes():
-        """Toggles UI for available snippets"""
+        """Toggles Available notes"""
         if gui.showing:
             gui.hide()
         else:
             gui.show()
     def hide_notes():
-        """Hides the snippet UI"""
+        """Hides notes"""
         gui.hide()
 
+    def export_notes():
+        """Exports all notes to a text file"""
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute("SELECT * FROM notes")
+        rows = c.fetchall()
+        with open(os.path.expanduser(text_path), "w") as f:
+            print("Exporting notes...") 
+            for row in rows:
+                f.write(f'{row[0]}: {row[1]}\n')
+                print(f'{row[0]}: {row[1]}')
+        conn.close()
